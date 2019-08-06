ccef5a47d36d0a246685e5167930151d0fafe80c
Devin-Moore <52828636+Devin-Moore@users.noreply.github.com>
2019-08-06 10:22:10 -0600

    Wrapped variable in IIFE
    Wrapped variable in IIFE and added 'add' and 'getAll' functions.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
+var pokemonRepository = (function () {
+  var repository = [
+    {name: 'Bulbasaur',
+     height: 0.7,
+     type: ['grass', 'poison']},
+    {name: 'Squirtle',
+     height: 0.5,
+     type: ['water']},
+    {name: 'Charmander',
+     height: 0.6,
+     type: ['fire']}
+  ];
+  function add(pokemon) {
+    repository.push(pokemon);
+  }
+  function getAll() {
+    return repository;
+  }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
-var repository = [
-  {name: 'Bulbasaur',
-   height: 0.7,
-   type: ['grass', 'poison']},
-  {name: 'Squirtle',
-   height: 0.5,
-   type: ['water']},
-  {name: 'Charmander',
-   height: 0.6,
-   type: ['fire']}
- ]
-repository.forEach(function(pokedexList) {
+var allPokemon = pokemonRepository.getAll();
+
+allPokemon.forEach(function(pokedexList) {
   if (pokedexList.height > 0.6) {
     document.write(pokedexList.name + ' (Height: '+pokedexList.height+') - Wow that\'s big <br>' )
   }
