f9452b3167140d286cf4f18fc9fc1fa461dbb95a
Devin-Moore <52828636+Devin-Moore@users.noreply.github.com>
2019-08-27 08:40:02 -0600

    Added addListItem and showDetails
    added addListItem and showDetails. Changed variables to include '$'.  Made adjustments due to variable name changes.

--- js/script.js
+++ js/script.js
@@ -3,32 +3,51 @@ var pokemonRepository = (function () {
     {name: 'Bulbasaur',
      height: 0.7,
      type: ['grass', 'poison']},
+
     {name: 'Squirtle',
      height: 0.5,
      type: ['water']},
+
     {name: 'Charmander',
      height: 0.6,
      type: ['fire']}
   ];
-  function add(pokemon) {
-    repository.push(pokemon);
+
+  function addListItem(pokemon) {
+    var $listItem = document.createElement('li');
+    var $button = document.createElement('button');
+    $button.innerText = pokemon.name;
+    $listItem.appendChild($button);
+    $container.appendChild($listItem);
+    $button.addEventListener('click', function(event) {
+      showDetails(event.target.innerText);
+    });
+  };
+
+  function showDetails(pokemonName) {
+    console.log(pokemonName)
+  };
+
+  function add(repository) {
+    repository.push(item);
   }
+
   function getAll() {
     return repository;
   }
+
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails
   };
 })();
 
-var allPokemon = pokemonRepository.getAll();
+var $allPokemon = pokemonRepository.getAll();
 
-allPokemon.forEach(function(pokedexList) {
-  if (pokedexList.height > 0.6) {
-    document.write(pokedexList.name + ' (Height: '+pokedexList.height+') - Wow that\'s big <br>' )
-  }
-  else {
-    document.write(pokedexList.name + ' (Height: '+pokedexList.height+') <br> ' )
-  }
+var $container = document.querySelector('.container');
+
+$allPokemon.forEach(function(pokemon) {
+  pokemonRepository.addListItem(pokemon);
 });
